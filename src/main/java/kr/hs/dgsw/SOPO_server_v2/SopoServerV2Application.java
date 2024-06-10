f56af2329e71565dcf8bc7ec69e43fb6b1002b25
김호준 <hojoon4868@gmail.com>
2024-06-10 23:15:16 +0900

    feat: init

---
+++ src/main/java/kr/hs/dgsw/SOPO_server_v2/SopoServerV2Application.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.SOPO_server_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopoServerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopoServerV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/hs/dgsw/SOPO_server_v2/SopoServerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.SOPO_server_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopoServerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
