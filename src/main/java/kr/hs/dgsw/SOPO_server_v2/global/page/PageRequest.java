6b21b052f3f85749472d1dd4d3f0aa53f7538284
김가영 <yeong.kmj@gmail.com>
2024-07-23 12:06:02 +0900

    feat: add page request

---
+++ src/main/java/kr/hs/dgsw/SOPO_server_v2/global/page/PageRequest.java
@@ -0,0 +1,19 @@
+package kr.hs.dgsw.SOPO_server_v2.global.page;
+
+public record PageRequest(
+        Long page,
+        Long size) {
+
+    public static PageRequest of(final long page, final long size) {
+        return new PageRequest(page, size);
+    }
+
+    public void validate() {
+        if(page<1) {
+            throw new IllegalArgumentException("Page must over than 0");
+        }
+        if(size<1) {
+            throw new IllegalArgumentException("Size must over than 0");
+        }
+    }
+}
