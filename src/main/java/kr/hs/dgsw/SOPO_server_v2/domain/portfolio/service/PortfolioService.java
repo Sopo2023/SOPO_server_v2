6c4f531bcb58e149dad73207dd2e6a0d10efcfa8
김가영 <yeong.kmj@gmail.com>
2024-09-10 18:06:43 +0900

    feat: 중간커밋

--- src/main/java/kr/hs/dgsw/SOPO_server_v2/domain/portfolio/service/PortfolioService.java
+++ src/main/java/kr/hs/dgsw/SOPO_server_v2/domain/portfolio/service/PortfolioService.java
@@ -2,9 +2,12 @@ package kr.hs.dgsw.SOPO_server_v2.domain.portfolio.service;
 
 import kr.hs.dgsw.SOPO_server_v2.domain.portfolio.entity.PortfolioEntity;
 import kr.hs.dgsw.SOPO_server_v2.domain.portfolio.enums.PortfolioState;
+import kr.hs.dgsw.SOPO_server_v2.domain.portfolio.presentation.dto.req.ModifyPortfolioReq;
 import kr.hs.dgsw.SOPO_server_v2.domain.portfolio.presentation.dto.res.LoadPortfolioRes;
 import kr.hs.dgsw.SOPO_server_v2.domain.portfolio.repository.PortfolioRepository;
+import kr.hs.dgsw.SOPO_server_v2.global.infra.security.GetCurrentMember;
 import kr.hs.dgsw.SOPO_server_v2.global.page.PageRequest;
+import kr.hs.dgsw.SOPO_server_v2.global.response.Response;
 import kr.hs.dgsw.SOPO_server_v2.global.response.ResponseData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +20,25 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class PortfolioService {
     private final PortfolioRepository portfolioRepository;
+    private final GetCurrentMember getCurrentMember;
+
+    public ResponseData<Long> generatePortfolio(){
+        PortfolioEntity portfolioEntity = portfolioRepository.save(
+                PortfolioEntity.builder()
+                        .portfolioContent(null)
+                        .portfolioTitle(null)
+                        .portfolioState(PortfolioState.PENDING)
+                        .tagEntity(null)
+                        .member(getCurrentMember.current())
+                        .build());
+
+        return ResponseData.of(HttpStatus.OK, "포트폴리오 생성 성공", portfolioEntity.getPortfolioId());
+    }
+
+    public Response modifyPortfolio(ModifyPortfolioReq generatePortfolioReq){
+        return null;
+
+    }
 
     public ResponseData<List<LoadPortfolioRes>> pagingPortfolio(PageRequest pageRequest){
         List<PortfolioEntity> portfolioEntities = portfolioRepository.findPortfoliosByState(PortfolioState.ACTIVE);
@@ -29,4 +51,5 @@ public class PortfolioService {
 
         return ResponseData.of(HttpStatus.OK, "성공", loadPortfolioReqList);
     }
+
 }
